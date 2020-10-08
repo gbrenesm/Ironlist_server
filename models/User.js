564d4f620bbf0ede6c3cb9404fbd4c8dfdbf323c
Guadalupe Brenes <brenesmesa@gmail.com>
2020-10-07 19:14:01 -0500

    Sesion and passport config. Auth controllers and routes

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { Schema, model} = require ("mongoose")
+
+const userSchema = new Schema(
+    {
+        username: String,
+        email: {
+            type: String,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String
+        },
+        tasks: [{
+            type: Schema.Types.ObjectId,
+            ref: "Task"
+        }]
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = model ("User", userSchema)
---
+++ routes/auth.routes.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const router = express.Router()
+
+const { signupProcess,
+        loginProcess,
+        currentUser,
+        logout} = require ("../controllers/auth")
+
+
+router.post("/signup", signupProcess)
+
+router.post("/login", loginProcess)
+
+router.get("/currentuser", currentUser)
+
+router.get("/logout", logout)
+
+module.exports = router
